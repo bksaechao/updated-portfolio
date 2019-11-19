3ca6a97568754da2e9fd5758b166b1aa94c7bd86
CaoKao <bksaechao@gmail.com>
2019-11-19 00:51:33 -0800

    formatted the about me section with media queries and inserted placeholder text and image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,4 @@
-$('#homeAnchor').click(function () {
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 1000);
-
-    return false;
-});
-
-$('#altAnchor').click(function () {
+$('a').click(function () {
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 1000);
