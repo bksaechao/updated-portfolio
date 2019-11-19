3b4a7f38996f75a4e81241afddb02df71d2f1f32
CaoKao <bksaechao@gmail.com>
2019-11-18 21:41:33 -0800

    added a rest.css, some centered welcome text , and a separate link to the contact page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,16 @@
-$('a').click(function () {
+$('#homeAnchor').click(function () {
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 1000);
+
     return false;
 });
+
+$('#altAnchor').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+
+    return false;
+});
+
